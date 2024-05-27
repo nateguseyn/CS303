bb8fc8b119085a47fbaeaffeb92d2e07d307c726
nateguseyn <natig.husseynzada@gmail.com>
2024-05-27 16:57:05 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Zəhmət olmasa, yoxlanılacaq ədədi daxil edin: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (IsPrime(number))
+        {
+            Console.WriteLine(number + " sadə ədəddir.");
+        }
+        else if (number == 1)
+        {
+            Console.WriteLine("1 nə sadə nə də mürəkkəb ədəddir.");
+        }
+        else
+        {
+            Console.WriteLine(number + " mürəkkəb ədəddir.");
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+        for (int i = 3; i <= boundary; i += 2)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
